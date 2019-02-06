8eaa0ec55b15d3e91a979b165aa11fe89eafebc6
Alvaro <visvaro@gmail.com>
2019-02-05 23:53:41 -0600

    filters css

--- src/components/explore/explore.js
+++ src/components/explore/explore.js
@@ -26,11 +26,20 @@ class Explore extends Component {
     return (
       <div className="wrapper">
         <div className="nav-bar">
-        
           <NavBar/>
         </div>
-        <div className="cards">
-          <Card/>
+        <div className="explore-body">
+          <div className="filters-container">
+            <h5>Filters: </h5>
+            <button className="btn active" > Show all</button>
+            <button className="btn" > Morning</button>
+            <button className="btn" > Night</button>
+            <button className="btn" > Low-price</button>
+            <button className="btn" > Medium-price</button>
+          </div>
+          <div className="cards">
+            <Card/>
+          </div>
         </div>
       
       </div>
